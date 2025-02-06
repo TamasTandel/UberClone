77f6bfefb54be0a8005b627c0a0a299719f33a78
jainil <tamastandel>
2025-02-06 13:50:20 +0530

    for deploy app on vercel

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 
-function connectToDb(){
-    mongoose.connect(process.env.DB_CONNECT).then(()=>{
-        console.log('conected to DB');
-    }).catch(err => console.log('err',err));
+function connectToDb() {
+    const mongoURI = process.env.MONGODB_URI || process.env.DB_CONNECT;
+
+    mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then(() => console.log('MongoDB connected'))
+        .catch(err => console.log('Error connecting to MongoDB:', err));
 }
 
 module.exports = connectToDb;
